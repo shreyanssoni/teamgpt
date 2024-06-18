c155d0097e31d45672c9d86de9469fa8d6d0513b
shreyanssoni <soni21.shreyans@gmail.com>
2024-06-18 15:10:14 +0530

    abcd

--- src/app/api/users/logout/route.tsx
+++ src/app/api/users/logout/route.tsx
@@ -1,13 +1,18 @@
 import { NextResponse } from 'next/server';
 import { permanentRedirect } from 'next/navigation';
+import { cookies } from 'next/headers';
 
 export async function GET(){
   try {
+    const cookieStore = cookies(); 
+    const myCookie = cookieStore.get("token");
+    // console.log("my cookies", myCookie);
+    cookies().set("token", ""); 
     const response = NextResponse.json({
         message: "Logout Successful"
     }, {status: 200})
     
-    response.cookies.set("token", "", {
+    response.cookies.set("token",'' , {
         httpOnly: true, expires: new Date(0)
     });
     
