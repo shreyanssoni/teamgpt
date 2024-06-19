f55402a24adebe791a7777593a779c89b9787eea
shreyanssoni <soni21.shreyans@gmail.com>
2024-06-19 18:30:23 +0530

    omg save

---
+++ src/app/contexts/ConversationContextProvider.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import ConversationContext from "./ConversationsContext";
+import axios from "axios";
+
+interface Message {
+  id: number;
+  content: string;
+}
+
+export const ConversationsContextProvider = ({ children }: any) => {
+  const [convos, setConvos] = useState(null);
+  const [token, setToken] = useState(null);
+  const [convoItem, setConvoItem] = useState([]);
+  const [messages, setMessages] = useState<Message[] | [[]]>([[]]);
+  const [message, setMessage] = useState("")
+
+  const getCookies = async () => {
+    const decodedToken: any = await axios.post("/api/getcookies", {
+      pass: "COOKIES",
+    });
+    setToken(decodedToken.data.content);
+    return decodedToken.data.content;
+  };
+
+  useEffect(() => {
+    getCookies();
+  }, []);
+
+  return (
+    <ConversationContext.Provider
+      value={{ convos, setConvos, 
+        token, setToken, 
+        convoItem, setConvoItem,
+        messages, setMessages,
+        message, setMessage
+    }}
+    >
+      {children}
+    </ConversationContext.Provider>
+  );
+};
---
+++ src/app/contexts/ConversationsContext.ts
@@ -0,0 +1,5 @@
+import React from "react";
+
+const ConversationContext = React.createContext<any | null >(null); 
+
+export default ConversationContext; 
